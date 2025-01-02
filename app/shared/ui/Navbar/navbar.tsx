d11168ea4a70fb6a936d385823d0b047fd2a44f8
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-03 01:55:14 +0330

    feat: create Navabr Component

--- app/shared/ui/Navbar/navbar.tsx
+++ app/shared/ui/Navbar/navbar.tsx
@@ -1,7 +1,29 @@
 import React from "react";
+import Logo from "@public/LogoNGN.svg";
+import Image from "next/image";
+import style from "./navbar.module.scss";
+import { Button, Group } from "@mantine/core";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className={style.wrapper}>
+      <ul className={style.navList}>
+        <li>
+          <Image src={Logo} alt="NGNlogo" />
+        </li>
+        <li>Home</li>
+        <li>Employers</li>
+        <li>Candidates</li>
+        <li>Events</li>
+        <li>Academy</li>
+        <li>About us</li>
+      </ul>
+      <Group className={style.btnGroup}>
+        <Button className={style.btnLogin}>Log in</Button>
+        <Button className={style.btnGet}>Get in Touch</Button>
+      </Group>
+    </div>
+  );
 };
 
 export default Navbar;
