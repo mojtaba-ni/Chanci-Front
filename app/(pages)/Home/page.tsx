8361519034c1ba6fdced8ce4d75c2ebe28ef3874
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-02 23:28:53 +0330

    feat:  initial structure of the project

---
+++ app/(pages)/Home/page.tsx
@@ -0,0 +1,12 @@
+import Land from "@/app/widget/Home/Land";
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <Land />
+    </div>
+  );
+};
+
+export default Home;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -19,13 +16,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="en" suppressHydrationWarning>
+      <body className={geistMono.variable}>
+        <MantineProvider>{children}</MantineProvider>
       </body>
     </html>
   );
