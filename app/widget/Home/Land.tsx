37f62309f15baccb1479a4d042685befd7c00050
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-02 23:31:20 +0330

    feat: initial Land Widget

---
+++ app/widget/Home/Land.tsx
@@ -0,0 +1,12 @@
+import Header from "@/app/shared/ui/Header/header";
+import React from "react";
+
+const Land = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default Land;
