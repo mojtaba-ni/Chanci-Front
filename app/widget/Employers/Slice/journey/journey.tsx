32afa953355fa99595254582af3500efda2c51d8
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-05 05:46:04 +0330

    feat: add Journey component with styles and features for startup guidance

---
+++ app/widget/Employers/Slice/journey/journey.tsx
@@ -0,0 +1,71 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import styles from "./journey.module.scss";
+import journey from "@public/image/file/invest.png";
+
+const Journey = () => {
+  return (
+    <div className={styles.journeySection}>
+      <div className={styles.imageWrapper}>
+        <div className={styles.imageContainer}>
+          <div className={styles.neonBorder}>
+            <div className={styles.glowEffect} />
+            <Image
+              src={journey}
+              alt="Journey Photo"
+              width={500}
+              height={400}
+              className={styles.image}
+            />
+            <div className={styles.bottomLine} />
+            <div className={styles.rightLine} />
+          </div>
+        </div>
+      </div>
+      <div className={styles.contentContainer}>
+        <h2>Navigating a Startup Journey?</h2>
+        <div className={styles.features}>
+          <div className={styles.feature}>
+            <div className={styles.dot} />
+            <p>Talent Acquisition: Get expert guidance on building your team</p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.dot} />
+            <p>Pitch Deck Assistance: Refine your pitch with our help</p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.dot} />
+            <p>Tech & Finance: Navigate complex decisions with ease</p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.dot} />
+            <p>
+              Support for Every Stage: Tailored solutions to fuel your startup's
+              growth.
+            </p>
+          </div>
+        </div>
+        <button className={styles.button}>
+          Book a free Consult
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          >
+            <path d="M5 12h14" />
+            <path d="m12 5 7 7-7 7" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Journey;
