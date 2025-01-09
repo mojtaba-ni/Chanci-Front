20ad58212e5d2c72ceb22c8ba49260ce88df7f6c
mojtaba_ni <mojtabanik@yahoo.com>
2025-01-09 14:06:31 +0330

    feat: update layout and navbar styles; add new page component

--- app/shared/ui/Navbar/navbar.tsx
+++ app/shared/ui/Navbar/navbar.tsx
@@ -8,45 +8,47 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className={style.wrapper}>
-      <div>
-        <ul className={style.navActions}>
-          <li className={style.menuBar}>
-            <Link href="/">
-              <Image src={Menu} alt="Menu" />
-            </Link>
-          </li>
-          <li>
-            <Link href="/">
-              <Image src={Logo} alt="NGNlogo" />
-            </Link>
-          </li>
-        </ul>
-        <ul className={style.navList}>
-          <li>
-            <Link href="/Home">Home</Link>
-          </li>
-          <li>
-            <Link href="/Employers">Employers</Link>
-          </li>
-          <li>
-            <Link href="/Condidates">Condidates</Link>
-          </li>
-          <li>
-            <Link href="/Events">Events</Link>
-          </li>
-          <li>
-            <Link href="/Academy">Academy</Link>
-          </li>
-          <li>
-            <Link href="/About">About us</Link>
-          </li>
-        </ul>
+    <div className={style.container}>
+      <div className={style.wrapper}>
+        <div>
+          <ul className={style.navActions}>
+            <li className={style.menuBar}>
+              <Link href="/">
+                <Image src={Menu} alt="Menu" />
+              </Link>
+            </li>
+            <li>
+              <Link href="/">
+                <Image src={Logo} alt="NGNlogo" />
+              </Link>
+            </li>
+          </ul>
+          <ul className={style.navList}>
+            <li>
+              <Link href="/Home">Home</Link>
+            </li>
+            <li>
+              <Link href="/Employers">Employers</Link>
+            </li>
+            <li>
+              <Link href="/Condidates">Condidates</Link>
+            </li>
+            <li>
+              <Link href="/Events">Events</Link>
+            </li>
+            <li>
+              <Link href="/Academy">Academy</Link>
+            </li>
+            <li>
+              <Link href="/About">About us</Link>
+            </li>
+          </ul>
+        </div>
+        <Group className={style.btnGroup}>
+          <Button className={style.btnLogin}>Log in</Button>
+          <Button className={style.btnGet}>Get in Touch</Button>
+        </Group>
       </div>
-      <Group className={style.btnGroup}>
-        <Button className={style.btnLogin}>Log in</Button>
-        <Button className={style.btnGet}>Get in Touch</Button>
-      </Group>
     </div>
   );
 };
